f7a82baa19a75c1550b12ea52d2f4bd902756132
Thangpn <thangpn.mc@gmail.com>
2020-05-13 08:58:45 +0700

    up

--- app/Http/Controllers/API/TasksController.php
+++ app/Http/Controllers/API/TasksController.php
@@ -22,33 +22,33 @@ class TasksController extends Controller
     {
         //
         // dd($request);
-        $request->validate([
-            // 'Ten' => 'required|unique:LoaiTin,Ten|min:3|max:15',
-            // 'name' => ['required', 'string', 'min:3|max:255'],
-            'name' => ['required','min:3','max:255'],
-            'content' => ['required','min:3','max:5000'],
-            'link_sound' => ['required','min:3','max:255'],
-        ],
-        [
-            'name.required'=>'Bạn chưa nhập tên task',
-            'name.min'=>'Bạn nhập quá ít ký tự',
-            'name.max'=>'Bạn nhập vượt quá số ký tự',
-            'content.required'=>'Bạn chưa nhập nội dung task',
-            'content.min'=>'Bạn nhập quá ít ký tự',
-            'content.max'=>'Bạn nhập vượt quá số ký tự',
-            'link_sound.required'=>'Bạn chưa nhập link âm thanh',
-            'link_sound.min'=>'Bạn nhập quá ít ký tự',
-            'link_sound.max'=>'Bạn nhập vượt quá số ký tự',
-        ]);
-        $task = new Tasks;
-        $task->name = $request->name;
-        $task->content = $request->content;
-        $task->link_sound = $request->link_sound;
-        $task->status =0;
-        $task->save();
-        return $task;
+        // $request->validate([
+        //     // 'Ten' => 'required|unique:LoaiTin,Ten|min:3|max:15',
+        //     // 'name' => ['required', 'string', 'min:3|max:255'],
+        //     'name' => ['required','min:3','max:255'],
+        //     'content' => ['required','min:3','max:5000'],
+        //     'link_sound' => ['required','min:3','max:255'],
+        // ],
+        // [
+        //     'name.required'=>'Bạn chưa nhập tên task',
+        //     'name.min'=>'Bạn nhập quá ít ký tự',
+        //     'name.max'=>'Bạn nhập vượt quá số ký tự',
+        //     'content.required'=>'Bạn chưa nhập nội dung task',
+        //     'content.min'=>'Bạn nhập quá ít ký tự',
+        //     'content.max'=>'Bạn nhập vượt quá số ký tự',
+        //     'link_sound.required'=>'Bạn chưa nhập link âm thanh',
+        //     'link_sound.min'=>'Bạn nhập quá ít ký tự',
+        //     'link_sound.max'=>'Bạn nhập vượt quá số ký tự',
+        // ]);
+        // $task = new Tasks;
+        // $task->name = $request->name;
+        // $task->content = $request->content;
+        // $task->link_sound = $request->link_sound;
+        // $task->status =0;
+        // $task->save();
+        // return $task;
 
-    }
+}
 
 
     public function show($id)
@@ -108,5 +108,32 @@ class TasksController extends Controller
         //
         $task = Tasks::where('id',$id)->delete();
 
+    }
+    public function xuly(){
+    $data= Tasks::all();
+    dd($data)
+;   $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => 'https://api.fpt.ai/hmi/tts/v5',
+      CURLOPT_CUSTOMREQUEST => 'POST',
+      CURLOPT_POSTFIELDS => 'a hi hi',
+      CURLOPT_HTTPHEADER => array(
+        'api-key: rGYRC6j4IJVCtO6wgIOkgyFUrr6TBVUS',
+        'speed: ',
+        'voice: male'
+      ),
+    ));
+
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+
+    curl_close($curl);
+
+    if ($err) {
+      echo 'cURL Error #:' . $err;
+    } else {
+      echo $response;
+    }
     }
 }
